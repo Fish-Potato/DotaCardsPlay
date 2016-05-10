3f3f6da7850efb79e3cc38070d5693af4f642e8c
xeostream <iostreamin@gmail.com>
2016-05-10 23:45:11 +0800

    add ehcache

--- src/com/zhaoqi/services/impl/MemberServiceImpl.java
+++ src/com/zhaoqi/services/impl/MemberServiceImpl.java
@@ -3,6 +3,9 @@ package com.zhaoqi.services.impl;
 import com.zhaoqi.mapper.MemberMapper;
 import com.zhaoqi.model.common.Member;
 import com.zhaoqi.services.IMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -16,8 +19,21 @@ public class MemberServiceImpl implements IMemberService {
     @Resource
     private MemberMapper memberMapper;
 
+    @Autowired
+    private EhCacheCacheManager cacheManager;
+
+    private final String CACHE_KEY_HEROES = "heroes";
+
     @Override
     public Member getById(int id) {
-        return memberMapper.getById(id);
+        Member member;
+        Cache cache = cacheManager.getCache(CACHE_KEY_HEROES);
+        if (null == cache.get(id, Member.class)) {
+            member = memberMapper.getById(id);
+            cache.put(id, member);
+        } else {
+            member = cache.get(id, Member.class);
+        }
+        return member;
     }
 }
